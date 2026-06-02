7be906f339ca4b3a0c334f2a1a0c956a07cb58a3
Angelikah Kalika <angelikahkalika@gmail.com>
2026-06-02 11:46:44 -0700

    stress meter

--- driving.js
+++ driving.js
@@ -11,3 +11,46 @@ const myTimer = setInterval(() => {
         console.log("Timer stopped.");
     }
 }, 1000);
+
+let timeLeft = 30;
+
+const timerDisplay = document.getElementById("timer");
+
+const countdown = setInterval(() => {
+    timeLeft--;
+
+    timerDisplay.textContent = `Time: ${timeLeft}`;
+
+    if (timeLeft <= 0) {
+        clearInterval(countdown);
+
+        alert("Game Over!");
+
+        // You could redirect to your game over page:
+        // window.location.href = "gameover.html";
+    }
+}, 1000);
+
+let stress = 0;
+
+setInterval(() => {
+    stress += 2;
+    document.getElementById("stressFill").style.width = stress + "%";
+}, 1000);
+
+let stress = 0;
+
+const stressFill = document.getElementById("stressFill");
+
+setInterval(() => {
+
+    if (stress < 100) {
+        stress += 5;
+        stressFill.style.width = stress + "%";
+    }
+
+    if (stress >= 100) {
+        alert("You are too stressed!");
+    }
+
+}, 1000);
