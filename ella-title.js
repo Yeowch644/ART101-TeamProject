364649c2adf4f5365db54ce9e02bd05618d8b44a
ella <ellag3255@gmail.com>
2026-06-02 22:00:17 -0700

    fix-ella

--- ella-title.js
+++ ella-title.js
@@ -1,6 +1,4 @@
-function startGame () {
+function startGame() {
     alert("The engine starts. Something is already watching you.");
-
-    // whatever the next page is called
-    window.location.href = " ";
+    window.location.href = "driving.html";
 }
