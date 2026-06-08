f52d2f918b771b1ed1f52aa26706e8595defafb9
Yeowch644 <jadentanyao644@gmail.com>
2026-06-08 02:13:45 -0700

    As good as it gets without more visuals

--- title.js
+++ title.js
@@ -1,3 +1,7 @@
 
 /* THIS IS THE JAVASCRIPT FILE FOR INDEX.HTML, THE STARTING SCREEN */
 
+function startGame() {
+    alert("The engine starts. Something is already watching you.");
+    window.location.href = "driving.html";
+}
