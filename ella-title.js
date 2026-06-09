d12246e4747d51d7364bebfa7e1135c790b4393c
Yeowch644 <jadentanyao644@gmail.com>
2026-06-08 21:31:54 -0700

    code labeling

--- ella-title.js
+++ ella-title.js
@@ -1,3 +1,6 @@
+// Ella's code
+// IMPLEMENTED IN MAIN CODE NOW
+
 function startGame() {
     alert("The engine starts. Something is already watching you.");
     window.location.href = "driving.html";
